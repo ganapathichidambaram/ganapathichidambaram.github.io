5893dc745821bcc31c35b41932edd3070229feaa
Ganapathi Chidambaram <ganapathi.rj@gmail.com>
2024-09-08 23:47:48 +0530

    update

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -11,6 +11,30 @@ export const metadata = {
     template: '%s - Your Gateway to Tech Excellence',
     default: 'Your Gateway to Tech Excellence',
   },
+  generator: 'TechnoBureau',
+  applicationName: 'TechnoBureau',
+  referrer: 'origin-when-cross-origin',
+  publisher: 'TechnoBureau',
+  creator: 'Ganapathi Chidambaram',
+  authors: [{ name: 'Ganapathi Chidambaram' }, { url: 'https://github.com/GanapathiChidambaram' }],
+
+  description: 'We offers a wealth of information, news, and insights into the ever-evolving world of technology. Whether you are a tech enthusiast, professional, or simply curious about the latest trends, TechnoBureau provides you with a one-stop hub to stay up-to-date and explore the fascinating realm of innovation.',
+  keywords: 'technobureau,linux,documentation,devops,kubernetes',
+  metadataBase: new URL('https://ganapathichidambaram.github.io'),
+  alternates: {
+    canonical: '/',
+  },
+  openGraph: {
+    images: '/favicon.ico',
+    title: 'Your Gateway to Tech Excellence',
+    description: 'We offers a wealth of information, news, and insights into the ever-evolving world of technology. Whether you are a tech enthusiast, professional, or simply curious about the latest trends, TechnoBureau provides you with a one-stop hub to stay up-to-date and explore the fascinating realm of innovation.',
+    url: 'https://ganapathichidambaram.github.io',
+    siteName: 'TechnoBureau',
+    type: 'website',
+    locale: 'en_US',
+    authors: ['Ganapathi Chidambaram'],
+  },
+  robots: 'index, follow',
 }
 
 export default async function RootLayout({ children }) {
